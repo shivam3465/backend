f4688c72ab0a687af87e97ff40bd3fcd3c98ebe3
shivam kumar <shivamkr19102000@gmail.com>
2023-05-13 18:32:24 +0530

    basic login logout and signup functionality

--- app..js
+++ app..js
@@ -1,17 +1,87 @@
-const express=require('express')
-const app = express();
-const portNo=5000;
-const bodyparser = require('body-parser');
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import path from "path";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
 
-app.get('/',(req,res)=>{
-    res.send("working")
+mongoose.connect('mongodb://localhost:27017/',{dbName:'backend'}).then(()=> console.log("database connected"))
+
+const userSchema=new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
 })
 
-app.get('/movies/:id',(req,res)=>{
-    res.send("about page")
-    console.log(req.body)
-    })
+const User=mongoose.model('users',userSchema);
 
-app.listen(portNo,()=>{
-    console.log("server listening on port ",portNo)
+
+const app=express();
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const authenticator=(req,res,next)=>{    
+    if(req.cookies.token){
+        next();
+    }
+    else{
+        res.redirect('/login');
+    }
+}
+
+app.get('/',authenticator,(req,res)=>{    
+    res.sendFile(path.join(path.resolve(),'/index.html'));
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(path.resolve(),'/register.html'));
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(path.resolve(),'/login.html'));
+})
+app.get('/logout',(req,res)=>{
+    res.cookie('token',"none",{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.redirect('/')
+})
+
+
+
+app.post('/register',async (req,res)=>{
+    const {name,email,password}=req.body;
+    const prevUser=await User.findOne({email});
+    
+    // console.log(prevUser);
+    if(!prevUser){
+        const hashedPassword=await bcrypt.hash(password,10);        
+        const user=await User.create({name,email,password:hashedPassword});   
+    }    
+    res.redirect('/')
+})
+
+app.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const user=await User.findOne({email});    
+    if(!user){        
+        res.redirect('/register');
+    }
+    else{
+        const id=jwt.sign(user.id,"adlfkkiowesoiviowod")
+        const isMatch= bcrypt.compare(password,user.password);
+        if(isMatch){
+            res.cookie('token',id,{
+                expires: new Date(Date.now()+ 1000*60*60),
+                httpOnly:true,
+            })
+            res.redirect('/');
+        }
+        else{
+            res.redirect('/login');
+        }
+    }
+})
+
+app.listen(5000,()=>{
+    console.log("server listening on port 5000")
 })
